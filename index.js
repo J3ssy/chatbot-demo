896996dbe986b002bb98cb3a5272dc9c3060d210
J3ssy <37377358+J3ssy@users.noreply.github.com>
2018-03-15 13:55:57 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -31,20 +31,29 @@ app.get("/webhook", function (req, res) {
 });
 
 // Todos eventos de mesenger sera apturados por esta ruta
-app.post("/webhook", function (req, res) {
-    // Verificar si el vento proviene del pagina asociada
-    if (req.body.object == "page") {
-        // Si existe multiples entradas entraas
-        req.body.entry.forEach(function(entry) {
-            // Iterara todos lo eventos capturados
-            entry.messaging.forEach(function(event) {
-                if (event.message) {
-                    process_event(event);
-                }
-            });
-        });
-        res.sendStatus(200);
-    }
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
 });
 
 
